6a57eea19baa5dfa6d95160cb5fe47f94b8f4ecb
lee <softcola6666@gmail.com>
2023-05-09 17:11:32 +0900

    뉴스 데이터만들기까지 성공/그러나 api데이터를 저장을 안하는 오류있음

--- src/main/java/com/invest/news/NewsController.java
+++ src/main/java/com/invest/news/NewsController.java
@@ -6,6 +6,7 @@ import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -46,4 +47,5 @@ public class NewsController {
 
         return futureArticles.get(); // 기사 목록을 반환합니다.
     }
+    
 }
