bb55f321c7b842571f40c8d17f4b90233de40268
Akisigure <daeone99@gmail.com>
2023-05-11 10:36:48 +0900

    Revert "Merge branch 'main' into LMW"
    This reverts commit cd2202bd486a5acd8a8c538a2c989a687fa89a7c, reversing
    changes made to 8a3d990cc83ea36969ac13d83770827b18b48efd.

--- src/main/java/com/invest/SeniorInvestApplication.java
+++ src/main/java/com/invest/SeniorInvestApplication.java
@@ -2,12 +2,7 @@ package com.invest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-
 
 @SpringBootApplication
 @EnableScheduling
@@ -16,10 +11,5 @@ public class SeniorInvestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeniorInvestApplication.class, args);
 	}
-	
-	@Bean
-	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-		return new HiddenHttpMethodFilter();
-	}
-	
+
 }
